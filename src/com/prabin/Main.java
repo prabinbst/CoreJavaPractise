5c0223fdd9426dc8904dbd594d66b7ae628df5c2
prabn <prabnbst@gmail.com>
2020-07-18 22:06:45 -0500

    Project initialized....

---
+++ src/com/prabin/Main.java
@@ -0,0 +1,9 @@
+package com.prabin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Good Night");
+        System.out.println("My first Java project");
+    }
+}
