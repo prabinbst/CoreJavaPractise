cb67d1bd4b5122ca502664e6a35a4078bff344e3
prabn <prabnbst@gmail.com>
2020-07-18 22:24:00 -0500

    New Packages Added

---
+++ src/com/prabin/day1/EmployeeRecords.java
@@ -0,0 +1,12 @@
+package com.prabin.day1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class EmployeeRecords {
+    public static void main(String[] args) {
+        String firstName = "Prabin";
+
+
+        System.out.println("First Name= "+firstName);
+    }
+}
