4fc2bac553dfc58449fecd9bdcef55cf49595b69
prabn <prabnbst@gmail.com>
2020-07-18 22:51:52 -0500

    Pratice java

---
+++ src/com/prabin/day2/Pratice1.java
@@ -0,0 +1,13 @@
+package com.prabin.day2;
+
+public class Pratice1 {
+    public static void main (String [] args) {
+        int a = 20;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Total=" + sum);
+
+
+    }
+
+}
