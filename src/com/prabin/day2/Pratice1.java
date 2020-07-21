57bc56d18e85ff8bf9a3a239d8c34e9e16ba7e24
prabn <prabnbst@gmail.com>
2020-07-20 23:11:21 -0500

    Pratice to push Github account

--- src/com/prabin/day2/Pratice1.java
+++ src/com/prabin/day2/Pratice1.java
@@ -4,10 +4,7 @@ import java.sql.SQLOutput;
 
 public class Pratice1 {
     public static void main (String [] args) {
-        int a = 20;
-        int b = 10;
-        int sum = a + b;
-        System.out.println("Total=" + sum);
+
 
         String fisrtName = "Chinu";
         String lastName = "Kattel";
